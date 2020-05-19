5932ac7e680b4358c027366611eea4a3f7914c1d
zhouf00 <49618748+zhouf00@users.noreply.github.com>
2020-05-19 23:05:51 +0800

    aaa

--- vue学习-v2/01-webpack的使用/02-webpack配置/webpack.config.js
+++ vue学习-v2/01-webpack的使用/02-webpack配置/webpack.config.js
@@ -6,3 +6,4 @@ module.exports = {
     filename: 'bundle.js'
   },
 }
+
---
+++ vue学习-v2/01-webpack的使用/05-webpack的Plugin/src/main.js
@@ -0,0 +1,26 @@
+const {add, mul} = require('./js/mathUtils');
+
+console.log(add(20, 30));
+
+import {name, age} from './js/info';
+
+console.log(name, age);
+
+require('./css/normal.css');
+
+require('./css/special.less');
+
+document.writeln('<h2>你好</h2>');
+
+// 使用Vue进行开发
+import Vue from 'vue'
+import App from './vue/App.vue'
+
+new Vue({
+  el:'#app',
+  template: '<App/>',
+  components: {
+    App
+  }
+})
+document.writeln('哈哈哈 ')
