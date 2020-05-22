b83cb01fd4ff2b123f23055bb1045b045d7e675e
zhouf <41719122@qq.com>
2020-05-22 15:07:46 +0800

    路由学习

---
+++ vue学习-v3/learnvuerouter/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>我是首页</h2>
+    <p>我是首页内容</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- vue学习-v3/learnvuerouter/src/router/index.js
+++ vue学习-v3/learnvuerouter/src/router/index.js
@@ -1,9 +1,12 @@
+// 配置路由相关的信息
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+// 通过vue.use（插件），安装插件
 Vue.use(Router)
 
+// 创建VueRouter对象
 export default new Router({
   routes: [
     {
