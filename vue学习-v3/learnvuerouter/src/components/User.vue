31bb3ebfa48bad2f4d14354e1d762c58054e8dc9
zhouf <41719122@qq.com>
2020-05-25 09:29:10 +0800

    homelearn

---
+++ vue学习-v3/learnvuerouter/src/components/User.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>我是用户界面</h2>
+    <p>我是用户相关内容</p>
+    <h2>{{userid}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'User',
+    computed: {
+      userid () {
+        // 拿到路由的id
+        return this.$route.params.userid
+      }
+    }
+  }
+  
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
