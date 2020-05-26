a202c0722c75f2e1213e7e39e38629ebf4260c71
zhouf <41719122@qq.com>
2020-05-26 11:47:59 +0800

    vue组件学习

---
+++ vue学习-v4/02-tabbar/src/App.vue
@@ -0,0 +1,69 @@
+<!-- app组件 -->
+<template>
+  <div id='app'>
+    <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        
+      </tab-bar-item>
+      <tab-bar-item>
+        
+      </tab-bar-item>
+      <tab-bar-item>
+        
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+  //例如：import 《组件名称》 from '《组件路径》';
+  import TabBar from './components/tabbar/TabBar'
+  import TabBarItem from './components/tabbar/TabBarItem'
+
+  export default {
+    name: 'App',
+    //import引入的组件需要注入到对象中才能使用
+    components: {
+      TabBar,
+      TabBarItem
+    },
+    data() {
+      //这里存放数据
+      return {
+        
+      };
+    },
+    //监听属性 类似于data概念
+    computed: {},
+    //监控data中的数据变化
+    watch: {},
+    //方法集合
+    methods: {
+    
+    },
+    //生命周期 - 创建完成（可以访问当前this实例）
+    created() {
+    
+    },
+    //生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() {
+    
+    },
+    beforeCreate() {}, //生命周期 - 创建之前
+    beforeMount() {}, //生命周期 - 挂载之前
+    beforeUpdate() {}, //生命周期 - 更新之前
+    updated() {}, //生命周期 - 更新之后
+    beforeDestroy() {}, //生命周期 - 销毁之前
+    destroyed() {}, //生命周期 - 销毁完成
+    activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+  }
+</script>
+<style scoped>
+  @import "./assets/css/base.css";
+
+ </style>
