b02f89b10ddb18a2a6b1a1df789b9f9f18654292
zhouf <41719122@qq.com>
2020-05-28 09:30:22 +0800

    vuex学习

---
+++ vue学习-v5/learnvuex/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h2>-----{{message}}----------</h2>
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="$store.state.counter++">+</button>
+    <button @click="$store.state.counter--">-</button>
+    <hello-vuex/>
+  </div>
+</template>
+
+<script>
+  import HelloVuex from './components/HelloVuex'
+  export default {
+    name: 'App',
+    components:{
+      HelloVuex
+    },
+    data () {
+      return {
+        message: '我是APP组件',
+        counter:0
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ vue学习-v5/learnvuex/src/components/HelloVuex.vue
@@ -0,0 +1,16 @@
+<!-- vuex学习 -->
+<template>
+  <div>
+    <h2>-----hello组件----------</h2>
+    <h2>{{$store.state.counter}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloVuex',
+  }
+</script>
+<style scoped>
+  
+</style>
