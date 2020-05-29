ece7134cfa5fc4bcf0adda05ca451509efc8b09e
zhouf00 <49618748+zhouf00@users.noreply.github.com>
2020-05-29 19:00:57 +0800

    vuex学习

--- vue学习-v5/learnvuex/src/App.vue
+++ vue学习-v5/learnvuex/src/App.vue
@@ -4,6 +4,15 @@
     <h2>{{$store.state.counter}}</h2>
     <button @click="addition">+</button>
     <button @click="subtraction">-</button>
+    <button @click="addCount(5)">+5</button>
+
+    <h2>-----App内容:getters相关信息----------</h2>
+    <p>{{$store.getters.powerCounter}}</p>
+    <p>{{$store.getters.more20stu}}</p>
+    <p>{{$store.getters.more20stuLength}}</p>
+    <p>{{$store.getters.moreAgeStu(29)}}</p>
+
+    <h2>-----hello Vuex内容----------</h2>
     <hello-vuex/>
   </div>
 </template>
@@ -22,12 +31,20 @@
       }
     },
     methods: {
+      // 返回store里的mutations的方法
       addition(){
         this.$store.commit('increment')
       },
       subtraction() {
         this.$store.commit('decrement')
+      },
+      // mutations的方法传参数
+      addCount (count) {
+        this.$store.commit('incrementCount', count)
       }
+    },
+    computed:{
+    
     }
   }
 </script>
--- vue学习-v5/learnvuex/src/store/index.js
+++ vue学习-v5/learnvuex/src/store/index.js
@@ -7,7 +7,15 @@ Vue.use(Vuex)
 // 2. 创建对象
 const store = new Vuex.Store({
   state: {
-    counter:1000
+    // 保存状态
+    counter:1000,
+    age: 18,
+    students:[
+      {id: 110, name: 'why', age: 18},
+      {id: 111, name: 'kobe', age: 24},
+      {id: 112, name: 'james', age: 30},
+      {id: 113, name: 'curry', age: 20},
+    ]
   },
   mutations:{
     // 方法
@@ -16,8 +24,37 @@ const store = new Vuex.Store({
     },
     decrement (state) {
       state.counter--
+    },
+    incrementCount (state, count) {
+      state.counter += count
     }
   },
+  actions: {
+    // 异步操作
+  },
+  getters: {
+    // 类计算属性
+    powerCounter(state){
+      return state.counter * state.counter
+    },
+    more20stu (state) {
+      return state.students.filter(s => s.age > 20)
+    },
+    more20stuLength(state, getters) {
+      return getters.more20stu.length
+    },
+    moreAgeStu (state) {
+      // return function (age) {
+      //   return state.students.filter(s => s.age > age)
+      // }
+      return  age => {
+        return state.students.filter(s => s.age > age)
+      }
+    }
+  },
+  modules: {
+    // 划分模块
+  }
 
 })
 
